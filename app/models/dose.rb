6265c5d103057b5da481d08d531cff206b6547da
Astrid Boisson <astrid.bdc@gmail.com>
2020-05-15 08:00:24 +0200

    ok base cocktail

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,5 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, presence: true
-  validates :cocktail_id, :ingredient_id, presence: true
-  validates :ingredient_id, uniqueness: { scope: :cocktail_id }
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
